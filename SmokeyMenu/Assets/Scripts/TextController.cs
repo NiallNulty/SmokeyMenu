0ce427c3bb2fcc898d77801af5235cb32546abe5
Niall <nnulty97@gmail.com>
2020-11-27 10:26:49 +0000

    almost finished

---
+++ SmokeyMenu/Assets/Scripts/TextController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+
+    public float speed = 0;
+    public float speedAccelerationPerSecond = 0.1f;
+    
+
+    public Color color;
+    public TextMesh text;
+    float t;
+    Vector3 startPosition;
+    Vector3 target;
+    float timeToReachTarget;
+    void Start()
+    {
+        text = GetComponent<TextMesh>();
+        text.color = color;
+        startPosition = transform.position;
+        target = new Vector3(transform.position.x, transform.position.y, -30f);
+        SetDestination(target, 4f);
+    }
+    void Update()
+    {
+        speed += speedAccelerationPerSecond * Time.deltaTime;
+        text.color = color;
+        color.a = speed;
+        t += Time.deltaTime / timeToReachTarget;
+        transform.position = Vector3.Lerp(startPosition, target, t);
+    }
+    public void SetDestination(Vector3 destination, float time)
+    {
+        t = 0;
+        startPosition = transform.position;
+        timeToReachTarget = time;
+        target = destination;
+    }
+}
